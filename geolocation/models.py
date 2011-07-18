b777c1bb620f24a3033f49bff3275d3fe31eeedb
Matthias Kestenholz <mk@spinlock.ch>
2011-07-18 14:34:18 +0200

    Store results of failed geocoding too

--- geolocation/models.py
+++ geolocation/models.py
@@ -28,8 +28,10 @@ class GeoLocation(models.Model):
     _data = JSONField(editable=False, blank=True, null=True)
 
     formatted_address = models.TextField(_('formatted address'), blank=True)
-    latitude = models.DecimalField(_('latitude'), max_digits=20, decimal_places=17)
-    longitude = models.DecimalField(_('longitude'), max_digits=20, decimal_places=17)
+    latitude = models.DecimalField(_('latitude'), max_digits=20, decimal_places=17,
+        blank=True, null=True)
+    longitude = models.DecimalField(_('longitude'), max_digits=20, decimal_places=17,
+        blank=True, null=True)
 
     class Meta:
         verbose_name = _('geolocation')
@@ -38,6 +40,8 @@ class GeoLocation(models.Model):
     objects = GeoLocationManager()
 
     def __unicode__(self):
+        if self.latitude is None or self.longitude is None:
+            return u'%s (failed)' % self.address
         return self.address
 
     def refresh_data(self, save=False):
@@ -45,19 +49,24 @@ class GeoLocation(models.Model):
             'http://maps.googleapis.com/maps/api/geocode/json',
             urllib.urlencode({
                 'sensor': 'false',
-                'address': self.address,
+                'address': self.address.encode('utf-8'),
                 }),
             ))
 
         result = urllib2.urlopen(request, timeout=2) # 2 seconds
-        self._data = simplejson.load(result)
+        self._data = simplejson.load(result, use_decimal=True)
 
-        results = self._data['results'][0] # take first result
-        location = results['geometry']['location']
+        try:
+            results = self._data['results'][0] # take first result
+            location = results['geometry']['location']
 
-        self.formatted_address = results['formatted_address']
-        self.latitude = location['lat']
-        self.longitude = location['lng']
+            self.formatted_address = results['formatted_address']
+            self.latitude = location['lat']
+            self.longitude = location['lng']
+        except IndexError:
+            self.formatted_address = u'geocoding failed'
+            self.latitude = None
+            self.longitude = None
 
         if save:
             self.save()
