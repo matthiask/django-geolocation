018e2f659c05011dba7b8a4edfd6e06438ac7df0
Matthias Kestenholz <mk@spinlock.ch>
2011-08-03 14:56:52 +0200

    Show geolocation objects in the administration interface

---
+++ geolocation/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from geolocation.models import GeoLocation
+
+
+admin.site.register(GeoLocation,
+    list_display=('address', 'formatted_address', 'created', 'latitude', 'longitude'),
+    search_fields=('address', 'formatted_address'),
+    )
